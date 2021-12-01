5b85fb0f18e1e0c3231ccb58b8a0b09b924f7871
Alisson7Neres <alisson.neres@hotmail.com>
2021-12-01 10:50:28 -0300

    banner

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,7 +12,6 @@ export default {
 
 <style scoped>
     #footer {
-        position: absolute;
         height: 200px;
         background-color: #222;
         border-top: 4px solid #111;
@@ -20,7 +19,6 @@ export default {
         display: flex;
         align-items: center;
         justify-content: center;
-        bottom: 0;
         width: 100%;
     }
 </style>
