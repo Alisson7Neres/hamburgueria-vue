e0c81c1e39f1c3575c68c7ff362714af189fac0b
Alisson7Neres <alisson.neres@hotmail.com>
2021-12-09 10:47:46 -0300

    Removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -33,7 +33,7 @@
                   {{s.tipo}}
                 </option>
             </select>
-            <button class="delete-btn">Cancelar</button>
+            <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
     </div>
@@ -45,7 +45,7 @@ export default {
   name: "Dashboard",
   data() {
     return {
-      burges: null,
+      burgers: null,
       burger_id: null,
       status: []
     }
@@ -69,6 +69,17 @@ export default {
       const data = await req.json();
       
       this.status = data;
+    },
+    async deleteBurger(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+      method: "DELETE"
+      });
+
+      const res = await req.json();
+
+      // msg
+
+      this.getPedidos();
     }
   },
   mounted() {
