7115ea25ab6b38378dac27fcf4282cfcd4177ae3
Alisson7Neres <alisson.neres@hotmail.com>
2022-02-22 09:35:36 -0300

    img

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,32 +1,71 @@
 <template>
-    <div id="main-banner">
-        <h1>Make Your Burger</h1>
-    </div>
+  <div id="main-banner">
+    <h1>Make Your Burger</h1>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Banner"
-}
+  name: "Banner",
+};
 </script>
 
 <style scoped>
-    #main-banner {
-        background-image: url('/img/burger.jpg');
-        background-position:  0 -250px;
-        background-size: cover;
-        height: 500px;
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        width: 100%;
-    }
+#main-banner {
+  background-image: url("/img/burger.jpg");
+  background-position: 0 -500px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  object-fit: cover;
+}
+@media only screen and (max-width: 1500px) {
+  #main-banner {
+    background-position: 0 -200px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    /* display: none;*/
+  }
+}
 
-    #main-banner h1 {
-        color: #FFF;
-        text-align: center;
-        font-size: 60px;
-        background-color: #222;
-        padding: 20px 40px;
-    }
+@media only screen and (max-width: 1040px) {
+  #main-banner {
+    background-position: 0 -100px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+  }
+}
+
+@media only screen and (max-width: 860px) {
+  #main-banner {
+    background-position: 0 -10px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+  }
+}
+
+#main-banner h1 {
+  color: #fff;
+  text-align: center;
+  font-size: 60px;
+  background-color: #222;
+  padding: 20px 40px;
+}
 </style>
